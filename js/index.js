073d82472600c798f805fa98ecdfe8bff091bbae
lcrumbley87 <lcrumbley87@gmail.com>
2022-03-08 15:29:43 -0500

    tuesday 1st commit

--- js/index.js
+++ js/index.js
@@ -6,12 +6,12 @@ function myFunction() {
     } else {
       x.style.display = "block";
     }
-  }
-  var heart_button = document.getElementById(‘heart-button’);
-heart_button.addEventListener(“click”, function() {
-let counter_value = document.querySelector(‘.counter’);
-let count = counter_value.innerHTML;
-counter = Number(count)+1;
-console.log(counter);
-counter_value.innerHTML=counter;
-});
+  };
+//   var heart_button = document.getElementById(‘heart-button’);
+// heart_button.addEventListener(“click”, function() {
+// let counter_value = document.querySelector(‘.counter’);
+// let count = counter_value.innerHTML;
+// counter = Number(count)+1;
+// console.log(counter);
+// counter_value.innerHTML=counter;
+// });
