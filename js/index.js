9a2fd142755e496701a1d00da7c6ca88604e4583
lcrumbley87 <lcrumbley87@gmail.com>
2022-03-08 20:53:11 -0500

    Fixxed commit

--- js/index.js
+++ js/index.js
@@ -7,11 +7,12 @@ function myFunction() {
       x.style.display = "block";
     }
   };
-//   var heart_button = document.getElementById(‘heart-button’);
-// heart_button.addEventListener(“click”, function() {
-// let counter_value = document.querySelector(‘.counter’);
-// let count = counter_value.innerHTML;
-// counter = Number(count)+1;
-// console.log(counter);
-// counter_value.innerHTML=counter;
-// });
+
+  var heart_button = document.getElementById("heart-button");
+heart_button.addEventListener('click', function() {
+let counter_value = document.querySelector(".counter");
+let count = counter_value.innerHTML;
+counter = Number(count)+1;
+console.log(counter);
+counter_value.innerHTML=counter;
+});
