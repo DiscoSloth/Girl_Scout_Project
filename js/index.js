0d59470ebce8f68407354ecb03312d0ab0d58075
lcrumbley87 <lcrumbley87@gmail.com>
2022-03-10 18:41:59 -0500

    Thrusday Commit

--- js/index.js
+++ js/index.js
@@ -1,3 +1,15 @@
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+};
+
+
+
+
 window.onload=function() {
 
 /* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
